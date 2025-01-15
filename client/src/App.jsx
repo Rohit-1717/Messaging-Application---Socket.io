b4d6b1bedbf4be6f90a2bade8c1de72142b6a50f
Rohit-1717 <rohitvishwakarma1717@gmail.com>
2025-01-15 10:11:43 +0530

    protected route fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import SettingPage from "./pages/SettingPage";
@@ -7,24 +7,33 @@ import ProfilePage from "./pages/ProfilePage";
 import HomePage from "./pages/HomePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
+import { Loader } from "lucide-react";
 
 function App() {
-  const { authUser, checkAuth } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
   console.log(authUser);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={authUser?  <HomePage />: <Navigate to= "/login" />} />
+        <Route path="/signup" element={!authUser?<SignupPage />:<Navigate to= "/" />} />
+        <Route path="/login" element={!authUser?<LoginPage />:<Navigate to= "/" />} />
         <Route path="/settings" element={<SettingPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={authUser?<ProfilePage />: <Navigate to= "/login" />} />
       </Routes>
     </>
   );
--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
@@ -13,6 +14,12 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",  
+    credentials:true
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoute);
